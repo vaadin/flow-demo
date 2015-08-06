fc1a2cc1e74155b43a4d7e46b8e84e85fae1a26e
Artur Signell <artur@vaadin.com>
2015-08-06 19:13:09 +0300

    Minesweeper using Element API

---
+++ demo-minesweeper-element/src/main/java/com/vaadin/hummingbird/demo/minesweeper/element/ElementMinesweeper.java
@@ -0,0 +1,119 @@
+package com.vaadin.hummingbird.demo.minesweeper.element;
+
+import com.vaadin.hummingbird.dom.Element;
+
+public class ElementMinesweeper {
+
+    private Element table = new Element("table");
+    private Minefield minefield = new Minefield();
+
+    public ElementMinesweeper(long seed, double mineDensity, int rows,
+            int cols) {
+        minefield.init(rows, cols, seed, mineDensity);
+        initDOM();
+    }
+
+    private void initDOM() {
+        for (int rowIndex = 0; rowIndex < minefield.getRows(); rowIndex++) {
+            Element tr = new Element("tr");
+            table.appendChild(tr);
+            for (int colIndex = 0; colIndex < minefield.getCols(); colIndex++) {
+                Element td = new Element("td");
+
+                final int thisRow = rowIndex;
+                final int thisCol = colIndex;
+
+                td.addEventListener("click", () -> {
+                    if (minefield.isMine(thisRow, thisCol)) {
+                        boom();
+                        revealAll();
+                    } else {
+                        reveal(thisRow, thisCol);
+                        if (isAllRevealed()) {
+                            success();
+                        }
+                    }
+                });
+                tr.appendChild(td);
+            }
+        }
+
+    }
+
+    private void boom() {
+        showMessage("BOOM! Reload to try again", "boom");
+    }
+
+    private void success() {
+        showMessage("Congratulations!", "success");
+    }
+
+    private void showMessage(String message, String className) {
+        Element div = new Element("div").setAttribute("class", className)
+                .setProperty("textContent", message);
+        getElement().getParent().appendChild(div);
+    }
+
+    /**
+     * Reveal the cell at the given coordinates
+     *
+     * @param row
+     *            the row
+     * @param col
+     *            the column
+     */
+    public void reveal(int row, int col) {
+        Element td = getTd(row, col);
+        if (isRevealed(td)) {
+            // Already revealed
+            return;
+        }
+        if (minefield.isMine(row, col)) {
+            td.setAttribute("class", "mine");
+        } else {
+            td.setAttribute("class", "empty");
+            int count = minefield.getNearbyCount(row, col);
+            if (count > 0) {
+                td.setProperty("textContent", count + "");
+            } else {
+                // Autoreveal
+                for (Point p : minefield.getNearbyPoints(row, col)) {
+                    reveal(p.getRow(), p.getCol());
+                }
+            }
+        }
+    }
+
+    private boolean isRevealed(Element td) {
+        return td.hasAttribute("class");
+    }
+
+    private boolean isAllRevealed() {
+        for (int row = 0; row < minefield.getRows(); row++) {
+            for (int col = 0; col < minefield.getCols(); col++) {
+                if (!isRevealed(getTd(row, col))
+                        && !minefield.isMine(row, col)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void revealAll() {
+        for (int r = 0; r < minefield.getRows(); r++) {
+            for (int c = 0; c < minefield.getCols(); c++) {
+                reveal(r, c);
+            }
+        }
+    }
+
+    private Element getTd(int row, int col) {
+        return table.getChild(row).getChild(col);
+    }
+
+    public Element getElement() {
+        return table;
+    }
+
+}
---
+++ demo-minesweeper-element/src/main/java/com/vaadin/hummingbird/demo/minesweeper/element/MinesweeperUI.java
@@ -0,0 +1,74 @@
+package com.vaadin.hummingbird.demo.minesweeper.element;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.hummingbird.dom.Element;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+
+public class MinesweeperUI extends UI {
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MinesweeperUI.class, productionMode = false)
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        long seed;
+        double mineDensity;
+        int rows, cols;
+
+        String seedParam = request.getParameter("seed");
+        if (seedParam != null) {
+            seed = Long.parseLong(seedParam);
+        } else {
+            seed = System.currentTimeMillis();
+        }
+
+        String mineDensityParam = request.getParameter("mineDensity");
+        if (mineDensityParam != null) {
+            mineDensity = Double.parseDouble(mineDensityParam);
+        } else {
+            mineDensity = 0.2;
+        }
+        String rowsParam = request.getParameter("rows");
+        if (rowsParam != null) {
+            rows = Integer.parseInt(rowsParam);
+        } else {
+            rows = 10;
+        }
+        String colsParam = request.getParameter("cols");
+        if (colsParam != null) {
+            cols = Integer.parseInt(colsParam);
+        } else {
+            cols = 10;
+        }
+
+        Element style = new Element("style");
+        style.setProperty("textContent", //
+                "   td {" + //
+                        "                background: grey;" + //
+                        "                border: 1px solid black;" + //
+                        "                width:20px;" + //
+                        "                height:20px;" + //
+                        "        }" + //
+                        "        .empty {" + //
+                        "                background: white;" + //
+                        "        }" + //
+                        "        .mine {" + //
+                        "                background: red;" + //
+                        "        }" + //
+                        "        td {" + //
+                        "                text-align: center;" + //
+                        "        }" + "");
+        getElement().appendChild(style);
+
+        ElementMinesweeper minesweeper = new ElementMinesweeper(seed,
+                mineDensity, rows, cols);
+        getElement().appendChild(minesweeper.getElement());
+    }
+
+}
