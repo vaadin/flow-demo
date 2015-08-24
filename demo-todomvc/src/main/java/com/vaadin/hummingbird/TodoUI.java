17b93fda3326ea0436155497d05bfc2df32e4d15
Leif Åstrand <leif@vaadin.com>
2015-08-24 10:04:31 +0300

    Don't use valo for TodoMVC

--- demo-todomvc/src/main/java/com/vaadin/hummingbird/TodoUI.java
+++ demo-todomvc/src/main/java/com/vaadin/hummingbird/TodoUI.java
@@ -2,14 +2,16 @@ package com.vaadin.hummingbird;
 
 import javax.servlet.annotation.WebServlet;
 
-import com.vaadin.annotations.StyleSheet;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
-@StyleSheet("TodoList.css")
+@Theme("todomvc")
+@Title("TodoMVC")
 public class TodoUI extends UI {
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = TodoUI.class, productionMode = false)
