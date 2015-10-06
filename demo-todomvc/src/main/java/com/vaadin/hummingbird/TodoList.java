666eba4299be4d0ad9ac5418234d4b1e8a8ae5f9
Leif Åstrand <leif@vaadin.com>
2015-10-06 12:56:02 +0300

    Use new typesafe model API in TodoList

--- demo-todomvc/src/main/java/com/vaadin/hummingbird/TodoList.java
+++ demo-todomvc/src/main/java/com/vaadin/hummingbird/TodoList.java
@@ -15,21 +15,56 @@
  */
 package com.vaadin.hummingbird;
 
-import java.util.Iterator;
 import java.util.List;
 
 import com.vaadin.annotations.TemplateEventHandler;
 import com.vaadin.hummingbird.kernel.Element;
-import com.vaadin.hummingbird.kernel.StateNode;
 import com.vaadin.ui.Template;
 
 public class TodoList extends Template {
+    public interface Todo {
+        public String getTitle();
+
+        public void setTitle(String title);
+
+        public boolean isCompleted();
+
+        public void setCompleted(boolean completed);
+
+        public boolean isEditing();
+
+        public void setEditing(boolean editing);
+    }
+
+    public interface TodoListModel extends Model {
+        public List<Todo> getTodos();
+
+        public void setTodos(List<Todo> todods);
+
+        public int getRemainingCount();
+
+        public void setRemainingCount(int remainingCount);
+
+        public boolean isHasNoCompleted();
+
+        public void setHasNoCompleted(boolean hasNoCompleted);
+
+        public boolean isHasNoRemaining();
+
+        public void setHasNoRemaining(boolean hasNoRemaining);
+
+        public boolean isHasNoTodos();
+
+        public void setHasNoTodos(boolean hasNoTodos);
+    }
+
     private Runnable recalculateRunnable = this::updateCounters;
 
     private int completeCount = 0;
 
     public TodoList() {
-        getNode().put("remainingCount", Integer.valueOf(0));
+        // Can't currently do this using the model api
+        getNode().getMultiValued("todos");
     }
 
     private void setNeedsRecount() {
@@ -37,112 +72,103 @@ public class TodoList extends Template {
     }
 
     @TemplateEventHandler
-    public void addTodo(String todo) {
-        StateNode todoNode = StateNode.create();
-        todoNode.put("title", todo);
-        getTodos().add(todoNode);
+    public void addTodo(String title) {
+        Todo todo = getModel().create(Todo.class);
+        todo.setTitle(title);
+        getTodos().add(todo);
         setNeedsRecount();
     }
 
-    private List<Object> getTodos() {
-        return getNode().getMultiValued("todos");
+    private List<Todo> getTodos() {
+        return getModel().getTodos();
     }
 
     public void setCompleted(int todoIndex, boolean completed) {
-        StateNode todoNode = (StateNode) getTodos().get(todoIndex);
-        setTodoCompleted(todoNode, completed);
+        setTodoCompleted(getTodos().get(todoIndex), completed);
     }
 
     @TemplateEventHandler
     protected void setTodoCompleted(Element element, boolean completed) {
-        setTodoCompleted(element.getNode(), completed);
+        setTodoCompleted(getTodoForElement(element), completed);
     }
 
-    private void setTodoCompleted(StateNode todoNode, boolean completed) {
-        if (completed && !todoNode.containsKey("completed")) {
-            completeCount++;
-            todoNode.put("completed", Boolean.TRUE);
-        } else if (!completed && todoNode.containsKey("completed")) {
-            completeCount--;
-            todoNode.remove("completed");
+    private Todo getTodoForElement(Element element) {
+        return getModel().wrap(element.getNode(), Todo.class);
+    }
+
+    private void setTodoCompleted(Todo todo, boolean completed) {
+        if (completed == todo.isCompleted()) {
+            return;
         }
+        if (completed) {
+            completeCount++;
+        } else {
+            completeCount--;
+        }
+        todo.setCompleted(completed);
         setNeedsRecount();
     }
 
     private void updateCounters() {
-        StateNode node = getNode();
+        TodoListModel model = getModel();
 
-        updateBoolean(node, completeCount == 0, "hasNoCompleted");
+        model.setHasNoCompleted(completeCount == 0);
 
         int todoCount = getTodos().size();
 
         int remainingCount = todoCount - completeCount;
-        updateBoolean(node, remainingCount == 0, "hasNoRemaining");
-        updateBoolean(node, todoCount == 0, "hasNoTodos");
+        model.setHasNoRemaining(remainingCount == 0);
+        model.setHasNoTodos(todoCount == 0);
 
-        Integer remainingObj = Integer.valueOf(remainingCount);
-        if (!remainingObj.equals(node.get("remainingCount"))) {
-            node.put("remainingCount", remainingObj);
-        }
-    }
-
-    private void updateBoolean(StateNode node, boolean value, String key) {
-        if (value && !node.containsKey(key)) {
-            node.put(key, Boolean.TRUE);
-        } else if (!value && node.containsKey(key)) {
-            node.remove(key);
-        }
+        model.setRemainingCount(remainingCount);
     }
 
     public boolean isCompleted(int todoIndex) {
-        StateNode todoNode = (StateNode) getTodos().get(todoIndex);
-        return todoNode.containsKey("completed");
+        return getTodos().get(todoIndex).isCompleted();
     }
 
     @TemplateEventHandler
     private void handleEditBlur(Element element) {
-        StateNode node = element.getNode();
-        System.out.println("Title changed to: " + node.get("title"));
+        Todo todo = getTodoForElement(element);
+        System.out.println("Title changed to: " + todo.getTitle());
     }
 
     @TemplateEventHandler
     protected void removeTodo(Element element) {
-        removeTodo(element.getNode());
+        removeTodo(getTodoForElement(element));
     }
 
-    private void removeTodo(StateNode todoNode) {
-        if (todoNode.containsKey("completed")) {
+    private void removeTodo(Todo todo) {
+        if (todo.isCompleted()) {
             completeCount--;
         }
-        getNode().getMultiValued("todos").remove(todoNode);
+        getTodos().remove(todo);
         setNeedsRecount();
     }
 
     @TemplateEventHandler
     public void clearCompleted() {
-        List<Object> todos = getNode().getMultiValued("todos");
-        Iterator<Object> iterator = todos.iterator();
-        while (iterator.hasNext()) {
-            StateNode todoNode = (StateNode) iterator.next();
-            if (todoNode.containsKey("completed")) {
-                iterator.remove();
-            }
-        }
+        List<Todo> todos = getTodos();
+        todos.removeIf(Todo::isCompleted);
         completeCount = 0;
         setNeedsRecount();
     }
 
     @TemplateEventHandler
     public void setAllCompleted(boolean completed) {
-        List<Object> todos = getNode().getMultiValued("todos");
-        for (Object object : todos) {
-            updateBoolean((StateNode) object, completed, "completed");
-        }
+        List<Todo> todos = getTodos();
+        todos.forEach(todo -> todo.setCompleted(completed));
+
         if (completed) {
             completeCount = todos.size();
         } else {
             completeCount = 0;
         }
         setNeedsRecount();
+    }
+
+    @Override
+    protected TodoListModel getModel() {
+        return (TodoListModel) super.getModel();
     }
 }
