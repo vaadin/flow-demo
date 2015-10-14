7223eb017e9cf719270539e1f8fe274104ced8b7
Olli Tietäväinen <ollit@vaadin.com>
2015-10-14 16:06:27 +0300

    added customer form made with polymer elements

--- demo-simplecrm/src/main/java/com/vaadin/hummingbird/Customers.java
+++ demo-simplecrm/src/main/java/com/vaadin/hummingbird/Customers.java
@@ -15,13 +15,55 @@
  */
 package com.vaadin.hummingbird;
 
-import com.vaadin.ui.Template;
 import com.vaadin.annotations.Bower;
 import com.vaadin.annotations.HTML;
 import com.vaadin.annotations.JavaScript;
+import com.vaadin.annotations.JavaScriptModule;
+import com.vaadin.annotations.TemplateEventHandler;
+import com.vaadin.hummingbird.kernel.Element;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.Page;
+import com.vaadin.ui.Template;
+import com.vaadin.ui.Grid.JS;
+
+import elemental.json.JsonArray;
 
 @JavaScript({"customers-snapshot.json","customers.js"})
-@HTML({"/VAADIN/bower_components/iron-icons/social-icons.html","/VAADIN/bower_components/iron-icons/maps-icons.html"})
-@Bower({"iron-icons","iron-icon","paper-menu","vaadin-grid"})
+@HTML({"vaadin://bower_components/iron-icons/social-icons.html","vaadin://bower_components/iron-icons/maps-icons.html"})
+@Bower({"paper-button","paper-item","paper-dropdown-menu","paper-radio-button","paper-radio-group","paper-input","iron-form","iron-icons","iron-icon","paper-menu","vaadin-grid"})
 public class Customers extends Template {
+	
+    @JavaScriptModule("customers.js")
+    public interface JS {
+        public void populate(String dataJson);
+    }
+	
+	private Grid customersGrid = new Grid();
+	
+	@Override
+	public void attach() {
+		super.attach();
+		customersGrid.addColumn("foo");
+		customersGrid.addRow("bar");
+	}
+
+	@TemplateEventHandler
+    public void getTestData() {
+		String res = "[{\"id\": 1,\"firstName\": \"Gabrielle\",\"lastName\": \"Patel\",\"birthDate\": \"1943-07-17\",\"status\": \"ImportedLead\",\"gender\": \"Female\",\"email\": \"gabrielle@patel.com\",\"location\": {\"lat\": 42.38090140840465,\"lon\": -71.10852736243358},\"persisted\": true}]";
+		// doesn't work
+		Page.getCurrent().getJavaScript().execute("console.log('here');alert('hello');populate("+res+");");
+		// doesn't work
+		getJS(JS.class).populate(res);
+	}
+	
+	@TemplateEventHandler
+	public void save(String input) {
+		System.out.println("Save: " + input);
+	}
+	
+	@TemplateEventHandler
+	public void delete(String input) {
+		System.out.println("Delete: " + input);
+	}
+	
 }
