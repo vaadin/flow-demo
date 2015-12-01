6bba19fd6d5b53732338ab1f1c88b4a7a1b372cd
Pekka Hyvönen <pekka@vaadin.com>
2015-12-01 10:05:41 +0200

    Type information into PolymerComponent, so that setters return correct component type

--- hummingbird-api-generator/lib/com/vaadin/hummingbird/polymer/PolymerComponent.java
+++ hummingbird-api-generator/lib/com/vaadin/hummingbird/polymer/PolymerComponent.java
@@ -1,65 +1,75 @@
 package com.vaadin.hummingbird.polymer;
 
-import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.vaadin.ui.AbstractSimpleDOMComponentContainer;
+import com.vaadin.ui.Component;
 
 @SuppressWarnings("serial")
-public abstract class PolymerComponent extends AbstractSimpleDOMComponentContainer {
+public abstract class PolymerComponent<T extends PolymerComponent<T>>
+        extends AbstractSimpleDOMComponentContainer {
 
-    public PolymerComponent setDisabled(boolean disabled) {
+    protected abstract T getThis();
+
+    public T setDisabled(boolean disabled) {
         setBooleanAttribute("disabled", disabled);
-        return this;
+        return getThis();
     }
 
-    public PolymerComponent setBooleanAttribute(String name, boolean value) {
+    public T setBooleanAttribute(String name, boolean value) {
         if (value) {
             getElement().setAttribute(name, "");
         } else {
             getElement().removeAttribute(name);
         }
-        return this;
+        return getThis();
     }
 
     public boolean getBooleanAttribute(String value) {
         return getElement().hasAttribute(value)
-                && (getElement().getAttribute(value).isEmpty()
-                    || Boolean.parseBoolean(getElement().getAttribute(value)));
+                && (getElement().getAttribute(value).isEmpty() || Boolean
+                        .parseBoolean(getElement().getAttribute(value)));
     }
 
-    public PolymerComponent setNoink(boolean noink) {
+    public T setNoink(boolean noink) {
         setBooleanAttribute("noink", noink);
-        return this;
-    }
-    public PolymerComponent setTabindex(int index) {
-        getElement().setAttribute("tabindex", "" + index);
-        return this;
+        return getThis();
     }
 
-    public PolymerComponent setAriaLabel(String label) {
+    public T setTabindex(int index) {
+        getElement().setAttribute("tabindex", "" + index);
+        return getThis();
+    }
+
+    public T setAriaLabel(String label) {
         getElement().setAttribute("aria-label", label);
-        return this;
+        return getThis();
     }
 
     /**
-     * @param attributes a list of attributes or attribute:values pairs.
-     *        examples:
-     *          setAttributes("foo bar")
-     *          setAttributes("foo:bar ; hello:bye")
+     * @param attributes
+     *            a list of attributes or attribute:values pairs. examples:
+     *            setAttributes("foo bar") setAttributes("foo:bar ; hello:bye")
      */
-    public PolymerComponent setAttributes(String attributes) {
+    public T setAttributes(String attributes) {
         for (String attr : attributes.trim().replace(" *([;:]) *", "$1")
-             .split("[; ]+")) {
-            MatchResult e = Pattern.compile(" *([\\w-]+)( *: *)?(.*)? *")
-                .matcher(attr);
-            
-            if (e.group(3) != null) {
-                getElement().setAttribute(e.group(1), e.group(3));
-            } else {
-                setBooleanAttribute(e.group(1), true);
+                .split("[; ]+")) {
+            Matcher e = Pattern.compile(" *([\\w-]+)( *: *)?(.*)? *")
+                    .matcher(attr);
+            if (e.matches()) {
+                if (e.group(3) != null) {
+                    getElement().setAttribute(e.group(1), e.group(3));
+                } else {
+                    setBooleanAttribute(e.group(1), true);
+                }
             }
         }
-        return this;
+        return getThis();
+    }
+
+    public T with(Component... components) {
+        addComponents(components);
+        return getThis();
     }
 }
