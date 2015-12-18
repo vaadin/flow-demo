0bc527ac5e38b663cc0776baa4abcaa5d20b7f08
Pekka Hyvönen <pekka@vaadin.com>
2015-12-18 23:05:03 +0200

    Added some samples on Vaadin ComboBox

--- demo-polymer-elements/src/main/java/com/vaadin/hummingbird/demo/vaadin/ComboBoxSample.java
+++ demo-polymer-elements/src/main/java/com/vaadin/hummingbird/demo/vaadin/ComboBoxSample.java
@@ -1,19 +1,99 @@
 package com.vaadin.hummingbird.demo.vaadin;
 
 import com.vaadin.hummingbird.demo.SampleBase;
+import com.vaadin.hummingbird.paper.PaperDialog;
+import com.vaadin.hummingbird.paper.PaperItem;
 import com.vaadin.hummingbird.vaadin.VaadinComboBox;
 import com.vaadin.ui.CssLayout;
 
 public class ComboBoxSample extends SampleBase {
 
+    private static final String[] OPTIONS = { "foo", "bar", "baz" };
+
     public ComboBoxSample() {
+        addBasic();
+        addPreselected();
+        addDisabled();
+        addValueChangeListener();
+    }
 
-        CssLayout basicSection = createHorizontalSection(this, "Basic");
+    private void addBasic() {
+        CssLayout section = createHorizontalSection(this, "Basic");
 
-        VaadinComboBox cb1 = new VaadinComboBox().setLabel("Foo, bar or baz");
+        VaadinComboBox comboBox = new VaadinComboBox()
+                .setLabel("Foo, bar or baz");
 
-        basicSection.addComponent(cb1);
+        addOptions(comboBox);
 
-        addComponent(basicSection);
+        section.addComponent(comboBox);
+
+        addComponent(section);
+    }
+
+    private void addPreselected() {
+        CssLayout section = createHorizontalSection(this, "Preselected Value");
+
+        VaadinComboBox comboBox = new VaadinComboBox()
+                .setLabel("Foo, bar or baz").setValue("baz");
+
+        addOptions(comboBox);
+
+        section.addComponent(comboBox);
+
+        addComponent(section);
+    }
+
+    private void addDisabled() {
+        CssLayout section = createHorizontalSection(this,
+                "Disabled (missing from element)");
+
+        VaadinComboBox comboBox = new VaadinComboBox()
+                .setLabel("Foo, bar or baz").setDisabled(true);
+
+        addOptions(comboBox);
+
+        section.addComponent(comboBox);
+
+        addComponent(section);
+    }
+
+    private void addValueChangeListener() {
+        CssLayout section = createHorizontalSection(this,
+                "With ValueChangeListener");
+
+        VaadinComboBox comboBox = new VaadinComboBox()
+                .setLabel("Foo, bar or baz");
+
+        addOptions(comboBox);
+
+        comboBox.getElement().addEventData("value-changed", "value");
+        comboBox.getElement().addEventListener("value-changed",
+                e -> section.addComponent(new PaperDialog()
+                        .with(new PaperItem().setTextContent(
+                                "Value Changed: " + e.get("value").asString()))
+                        .open()));
+
+        section.addComponent(comboBox);
+
+        addComponent(section);
+    }
+
+    private void addOptions(VaadinComboBox comboBox) {
+        addItems(comboBox, OPTIONS);
+    }
+
+    private void addItems(VaadinComboBox comboBox, String... items) {
+        StringBuilder sb = new StringBuilder("$0.items = [");
+        for (int i = 0; i < items.length; i++) {
+            if (i > 0) {
+                sb.append(",");
+            }
+            sb.append("'");
+            sb.append(items[i]);
+            sb.append("'");
+        }
+        sb.append("];");
+        comboBox.getElement().getNode().enqueueRpc(sb.toString(),
+                comboBox.getElement());
     }
 }
