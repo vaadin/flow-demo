98ab0eed8496f18d32d7ebf022d81c6c29cda032
Artur Signell <artur@vaadin.com>
2016-02-24 14:29:59 +0200

    Hello world, initial version

---
+++ demo-hello-world-element/src/main/java/com/vaadin/hummingbird/demo/helloworld/element/HelloWorldUI.java
@@ -0,0 +1,34 @@
+package com.vaadin.hummingbird.demo.helloworld.element;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.hummingbird.dom.Element;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+
+public class HelloWorldUI extends UI {
+
+    @WebServlet(urlPatterns = "/*", name = "UIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = HelloWorldUI.class, productionMode = false)
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        Element textInput = new Element("input").setAttribute("id", "inputId")
+                .setAttribute("placeholder", "Enter your name");
+
+        Element button = new Element("button").setTextContent("Say hello");
+        button.addEventListener("click", e -> {
+            String inputValue = e.getEventData().getString("inputId.value");
+            Element helloText = new Element("div")
+                    .setTextContent("Hello " + inputValue);
+            getElement().appendChild(helloText);
+        } , "inputId.value");
+
+        getElement().appendChild(textInput, button);
+    }
+
+}
