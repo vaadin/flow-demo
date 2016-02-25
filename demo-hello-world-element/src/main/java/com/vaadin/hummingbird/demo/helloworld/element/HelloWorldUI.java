206952fbfa43166500d3078a4d8e3d5c38a9b534
Artur Signell <artur@vaadin.com>
2016-02-25 14:53:04 +0200

    Add missing javadoc and license headers
    Closes #30

--- demo-hello-world-element/src/main/java/com/vaadin/hummingbird/demo/helloworld/element/HelloWorldUI.java
+++ demo-hello-world-element/src/main/java/com/vaadin/hummingbird/demo/helloworld/element/HelloWorldUI.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.vaadin.hummingbird.demo.helloworld.element;
 
 import javax.servlet.annotation.WebServlet;
@@ -8,8 +23,15 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.UI;
 
+/**
+ * UI which demonstrates how a simple Hello World type application can be
+ * created.
+ */
 public class HelloWorldUI extends UI {
 
+    /**
+     * The main servlet for the application.
+     */
     @WebServlet(urlPatterns = "/*", name = "UIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = HelloWorldUI.class, productionMode = false)
     public static class Servlet extends VaadinServlet {
--- demo-minesweeper-element/src/main/java/com/vaadin/hummingbird/demo/minesweeper/element/ElementMinesweeper.java
+++ demo-minesweeper-element/src/main/java/com/vaadin/hummingbird/demo/minesweeper/element/ElementMinesweeper.java
@@ -1,20 +1,54 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.vaadin.hummingbird.demo.minesweeper.element;
 
 import java.util.Set;
 
 import com.vaadin.hummingbird.dom.Element;
 
+/**
+ * The Minesweeper UI component with the root element returned by
+ * {@link #getElement()}.
+ */
 public class ElementMinesweeper {
 
     private Element table = new Element("table");
     private Minefield minefield = new Minefield();
 
+    /**
+     * Creates a component and sets up a minefield using the given parameters.
+     *
+     * @param seed
+     *            the random seed to use when placing mines
+     * @param mineDensity
+     *            the ratio of mines to cells, between 0 and 1
+     * @param rows
+     *            the number of rows in the minefield
+     * @param cols
+     *            the number of columns in the minefield
+     */
     public ElementMinesweeper(long seed, double mineDensity, int rows,
             int cols) {
         minefield.init(rows, cols, seed, mineDensity);
         initDOM();
     }
 
+    /**
+     * Initializes the DOM for the component.
+     */
     private void initDOM() {
         for (int rowIndex = 0; rowIndex < minefield.getRows(); rowIndex++) {
             Element tr = new Element("tr");
@@ -31,6 +65,14 @@ public class ElementMinesweeper {
         }
     }
 
+    /**
+     * Handler called when the user clicks on a cell.
+     *
+     * @param row
+     *            the row which was clicked
+     * @param col
+     *            the column which was clicked
+     */
     private void cellClick(int row, int col) {
         if (minefield.isMine(row, col)) {
             boom();
@@ -44,10 +86,16 @@ public class ElementMinesweeper {
 
     }
 
+    /**
+     * Shows a message indicating mine explosion and failure.
+     */
     private void boom() {
         showMessage("BOOM! Reload to try again", "boom");
     }
 
+    /**
+     * Shows a message indicating the minefield was successfully cleared.
+     */
     private void success() {
         showMessage("Congratulations!", "success");
     }
@@ -88,11 +136,23 @@ public class ElementMinesweeper {
         }
     }
 
+    /**
+     * Checks if the cell represented by the given element has been revealed.
+     *
+     * @param td
+     *            the cell element
+     * @return true if the cell has been revealed, false otherwise
+     */
     private static boolean isRevealed(Element td) {
         Set<String> classList = td.getClassList();
         return classList.contains("mine") || classList.contains("empty");
     }
 
+    /**
+     * Checks if all empty cells have been revealed.
+     *
+     * @return true if all cells have been revealed, false otherwise.
+     */
     private boolean isAllRevealed() {
         for (int row = 0; row < minefield.getRows(); row++) {
             for (int col = 0; col < minefield.getCols(); col++) {
@@ -105,6 +165,9 @@ public class ElementMinesweeper {
         return true;
     }
 
+    /**
+     * Reveal all cells, regardless of whether they contain mines or not.
+     */
     private void revealAll() {
         for (int r = 0; r < minefield.getRows(); r++) {
             for (int c = 0; c < minefield.getCols(); c++) {
@@ -113,10 +176,24 @@ public class ElementMinesweeper {
         }
     }
 
+    /**
+     * Gets the element representing the given cell.
+     *
+     * @param row
+     *            the row coordinate
+     * @param col
+     *            the column coordinate
+     * @return the TD element representing the cell
+     */
     private Element getTd(int row, int col) {
         return table.getChild(row).getChild(col);
     }
 
+    /**
+     * Gets the root element for this component.
+     *
+     * @return the root element
+     */
     public Element getElement() {
         return table;
     }
