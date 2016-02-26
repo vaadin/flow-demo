5f1cf725d5bc8b59f68dc1f75df3c7ceeacf469b
Artur Signell <artur@vaadin.com>
2016-02-26 15:04:43 +0200

    TestBench tests for Hello World and Minesweeper
    Closes #31

---
+++ demo-minesweeper-element/src/test/java/com/vaadin/hummingbird/demo/minesweeper/element/MinesweeperIT.java
@@ -0,0 +1,55 @@
+package com.vaadin.hummingbird.demo.minesweeper.element;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class MinesweeperIT extends MinesweeperPageObject {
+
+    @Test
+    public void clickOnEmptyRevealsCell() {
+        open("seed=1");
+        assertCellHidden(0, 0);
+        getCell(0, 0).click();
+        assertCellEmpty(0, 0);
+    }
+
+    @Test
+    public void clickOnMineShowsNotification() {
+        open("seed=1");
+        getCell(9, 0).click();
+        waitForBoomNotification();
+    }
+
+    @Test
+    public void solveSeed1Minefield() {
+        open("seed=1&rows=10&cols=10");
+        for (int row = 0; row < 10; row++) {
+            for (int col = 0; col < 10; col++) {
+                if (!isMineSeed1(row, col)) {
+                    assertCellHiddenOrEmpty(
+                            row + "," + col + " is revealed or a mine", row,
+                            col);
+                    getCell(row, col).click();
+                }
+            }
+        }
+        waitForSuccessNotification();
+    }
+
+    private Minefield minefieldSeed1 = new Minefield();
+
+    {
+        minefieldSeed1.init(10, 10, 1, 0.2);
+    }
+
+    private boolean isMineSeed1(int row, int col) {
+        return minefieldSeed1.isMine(row, col);
+    }
+
+    @Override
+    protected WebElement getMineField() {
+        return findElement(By.cssSelector("table"));
+    }
+
+}
