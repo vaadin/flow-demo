e3a610aecd2b8f67b01c914d12c73df0beb87b01
Leif Åstrand <leif@vaadin.com>
2016-03-04 15:51:39 +0200

    Add initial web site

---
+++ demo-web-site/src/main/java/com/vaadin/hummingbird/demo/website/AboutLayout.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.hummingbird.demo.website;
+
+import com.vaadin.hummingbird.dom.Element;
+import com.vaadin.ui.HasSubView;
+import com.vaadin.ui.View;
+
+public class AboutLayout extends SimpleView implements HasSubView {
+
+    public AboutLayout() {
+        super(new Element("div"));
+        getElement().appendChild(new Element("p")
+                .setTextContent("Special layout for the about page."));
+    }
+
+    @Override
+    public void setSubView(View content) {
+        Element contentElement = content.getElement();
+        Element element = getElement();
+
+        if (element.getChildCount() == 2) {
+            Element lastChild = element.getChild(1);
+            if (lastChild.equals(contentElement)) {
+                return;
+            }
+
+            lastChild.removeFromParent();
+        }
+
+        element.appendChild(contentElement);
+    }
+
+}
