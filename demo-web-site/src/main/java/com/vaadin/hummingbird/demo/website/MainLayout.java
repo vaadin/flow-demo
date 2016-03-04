f3c1ccc1a245fe3d1eff9768b18183fe5316329f
Leif Åstrand <leif@vaadin.com>
2016-03-04 15:51:39 +0200

    Add initial web site

---
+++ demo-web-site/src/main/java/com/vaadin/hummingbird/demo/website/MainLayout.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.hummingbird.demo.website;
+
+import com.vaadin.hummingbird.dom.Element;
+import com.vaadin.hummingbird.router.HasSubView;
+import com.vaadin.hummingbird.router.View;
+import com.vaadin.shared.ApplicationConstants;
+
+/**
+ * Layout showing the main menu above a sub view.
+ *
+ * @since
+ * @author Vaadin Ltd
+ */
+public class MainLayout extends SimpleView implements HasSubView {
+
+    private final Element contentHolder = new Element("div");
+
+    /**
+     * Creates a new layout.
+     */
+    public MainLayout() {
+        super(new Element("div"));
+
+        Element menu = createMenu();
+
+        getElement().appendChild(Element.createText("My site"), menu,
+                contentHolder);
+        contentHolder.getClassList().add("content");
+
+        // Placeholder content
+        contentHolder.appendChild(new Element("div"));
+    }
+
+    private Element createMenu() {
+        Element menu = new Element("div");
+        menu.getClassList().add("menu");
+
+        // Configuring menu based on router configuration added in a separate PR
+        menu.appendChild(createMenuLink("Home", ""),
+                createMenuLink("About", "about"),
+                createMenuLink("Dynamic 1", "dynamic/one"),
+                createMenuLink("Dynamic 2", "dynamic/two"));
+        return menu;
+    }
+
+    private static Element createMenuLink(String caption, String path) {
+        Element link = new Element("a");
+
+        link.setTextContent(caption);
+        link.setAttribute("href", path);
+        link.setAttribute(ApplicationConstants.ROUTER_LINK_ATTRIBUTE, "");
+
+        return link;
+    }
+
+    @Override
+    public void setSubView(View content) {
+        Element contentElement = content.getElement();
+
+        contentHolder.setChild(0, contentElement);
+    }
+}
