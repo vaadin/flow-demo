3eaf30bf0001ddaf83ce9c0b41927e826f076551
Artur Signell <artur@vaadin.com>
2016-03-22 15:15:50 +0200

    Dynamic menu demo using products and categories

---
+++ demo-dynamic-menu/src/main/java/com/vaadin/hummingbird/demo/dynamicmenu/data/Category.java
@@ -0,0 +1,30 @@
+package com.vaadin.hummingbird.demo.dynamicmenu.data;
+
+import java.io.Serializable;
+
+public class Category implements Serializable {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
