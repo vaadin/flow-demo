76258de3caac9c0cfde900a3272c59e0140e58b8
Artur <artur@vaadin.com>
2016-04-14 10:51:50 +0300

    Stop using VAADIN in demo projects (#78)

--- demo-minesweeper-element/src/main/java/com/vaadin/hummingbird/demo/minesweeper/element/MinesweeperUI.java
+++ demo-minesweeper-element/src/main/java/com/vaadin/hummingbird/demo/minesweeper/element/MinesweeperUI.java
@@ -69,7 +69,7 @@ public class MinesweeperUI extends UI {
             cols = 10;
         }
 
-        getPage().addStyleSheet("VAADIN/minesweeper.css");
+        getPage().addStyleSheet("minesweeper.css");
 
         ElementMinesweeper minesweeper = new ElementMinesweeper(seed,
                 mineDensity, rows, cols);
