5b0600f78bd6d36db465f68437f4f0d957ceb3ba
Artur Signell <artur@vaadin.com>
2016-04-26 20:34:38 +0300

    Use same Travis scripts as main project except demos do not run javadoc
    Use PhantomJS from Maven

--- .travis.script.sh
+++ .travis.script.sh
@@ -1,25 +1,18 @@
 #!/usr/bin/env bash
 
-# Use PhantomJS 2.1.1 installed in before_install
-export PATH=$PWD/phantomjs-2.1.1-linux-x86_64/bin:$PATH
-
 # TRAVIS_PULL_REQUEST == "false" for a normal branch commit, the PR number for a PR
 # TRAVIS_BRANCH == target of normal commit or target of PR
 # TRAVIS_SECURE_ENV_VARS == true if encrypted variables, e.g. SONAR_HOST is available
 # TRAVIS_REPO_SLUG == the repository, e.g. vaadin/vaadin
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]
 then
 	# Pull request for master with secure vars (SONAR_GITHUB_OAUTH, SONAR_HOST) available
 
 	# Trigger Sonar analysis
 	echo "Running Sonar"
-	mvn -B -e -V -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Dtest.excludegroup= -Dsonar.verbose=true -Dsonar.analysis.mode=issues -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.host.url=$SONAR_HOST -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH -Dsonar.login=$SONAR_LOGIN -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar
+	mvn -B -e -V -Dmaven.javadoc.skip=false -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Dtest.excludegroup= -Dsonar.verbose=true -Dsonar.analysis.mode=issues -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.host.url=$SONAR_HOST -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH -Dsonar.login=$SONAR_LOGIN -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar
 else
-	# Something else than a pull request inside the repository OR a pull request for another branch than master
-	mvn -B -e -V -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Dtest.excludegroup= verify
+	# Something else than a "safe" pull request
+	mvn -B -e -V -Dmaven.javadoc.skip=false -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Dtest.excludegroup= verify javadoc:javadoc
 fi
-
-
-
-
