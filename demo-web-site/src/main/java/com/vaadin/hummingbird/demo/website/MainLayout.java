c1c6b450e5c89473c0bd498f7904f3ea541b89ff
Artur Signell <artur@vaadin.com>
2016-04-29 19:46:24 +0300

    Use @child@ to define child view location

--- demo-web-site/src/main/java/com/vaadin/hummingbird/demo/website/MainLayout.java
+++ demo-web-site/src/main/java/com/vaadin/hummingbird/demo/website/MainLayout.java
@@ -15,8 +15,6 @@
  */
 package com.vaadin.hummingbird.demo.website;
 
-import com.vaadin.hummingbird.dom.Element;
-import com.vaadin.hummingbird.router.HasChildView;
 import com.vaadin.hummingbird.router.View;
 import com.vaadin.ui.Template;
 import com.vaadin.ui.UI;
@@ -27,38 +25,12 @@ import com.vaadin.ui.UI;
  * @since
  * @author Vaadin Ltd
  */
-public final class MainLayout extends Template implements View, HasChildView {
-
-    private final Element contentHolder;
+public final class MainLayout extends Template implements View {
 
     /**
      * Creates a new layout.
      */
     public MainLayout() {
         UI.getCurrent().getPage().addStyleSheet("css/site.css");
-        contentHolder = findContentHolder();
-
-    }
-
-    private Element findContentHolder() {
-        for (int i = 0; i < getElement().getChildCount(); i++) {
-            Element child = getElement().getChild(i);
-            if (child.isTextNode()) {
-                continue;
-            }
-            if ("contentholder".equals(child.getAttribute("id"))) {
-                return child;
-            }
-        }
-        throw new IllegalStateException(
-                "No content holder defined in the template");
-    }
-
-    @Override
-    public void setChildView(View content) {
-        contentHolder.removeAllChildren();
-        if (content != null) {
-            contentHolder.appendChild(content.getElement());
-        }
     }
 }
