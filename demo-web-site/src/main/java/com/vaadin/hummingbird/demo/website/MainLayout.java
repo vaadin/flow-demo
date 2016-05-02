633a794da1e9f86f4161dd31567c09d0a037a44f
Pekka Hyvönen <pekka@vaadin.com>
2016-05-02 15:34:48 +0300

    Use for-loop in menu template for demo-website.
    Closes #103

--- demo-web-site/src/main/java/com/vaadin/hummingbird/demo/website/MainLayout.java
+++ demo-web-site/src/main/java/com/vaadin/hummingbird/demo/website/MainLayout.java
@@ -15,6 +15,11 @@
  */
 package com.vaadin.hummingbird.demo.website;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.vaadin.hummingbird.nodefeature.ModelMap;
 import com.vaadin.hummingbird.router.View;
 import com.vaadin.ui.Template;
 import com.vaadin.ui.UI;
@@ -28,9 +33,67 @@ import com.vaadin.ui.UI;
 public final class MainLayout extends Template implements View {
 
     /**
-     * Creates a new layout.
+     * Model for menu items.
+     */
+    public static class MenuItem implements Serializable {
+        private final String href;
+        private final String caption;
+
+        /**
+         * Creates a new menu item.
+         *
+         * @param href
+         *            the href to link to
+         * @param caption
+         *            the caption to display
+         */
+        public MenuItem(String href, String caption) {
+            this.href = href;
+            this.caption = caption;
+        }
+
+        /**
+         * Gets the href of the menu item.
+         *
+         * @return the href
+         */
+        public String getHref() {
+            return href;
+        }
+
+        /**
+         * Gets the caption of the menu item.
+         *
+         * @return the caption
+         */
+        public String getCaption() {
+            return caption;
+        }
+    }
+
+    /**
+     * Creates a new layout and menu.
      */
     public MainLayout() {
         UI.getCurrent().getPage().addStyleSheet("css/site.css");
+
+        createMenu();
+    }
+
+    private void createMenu() {
+        List<MenuItem> items = getMenuItems();
+
+        ModelMap model = getElement().getNode().getFeature(ModelMap.class);
+
+        model.setValue("items", items);
+    }
+
+    private List<MenuItem> getMenuItems() {
+        ArrayList<MenuItem> items = new ArrayList<>();
+        items.add(new MenuItem("about", "About"));
+        items.add(new MenuItem("param/1", "Parameter view"));
+        items.add(new MenuItem("resource/", "Resource view"));
+        items.add(new MenuItem("dynresource", "Dynamic resource view"));
+        return items;
     }
 }
