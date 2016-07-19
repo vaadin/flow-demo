84239c59f1b0d34e73f3bddcaf914b0619d9fe85
Artur Signell <artur@vaadin.com>
2016-07-19 15:02:30 +0300

    Change progress bubble demo into a Polygit web component demo

--- demo-web-component-using-polygit/src/main/java/com/vaadin/hummingbird/demo/webcomponent/polygit/MainView.java
+++ demo-web-component-using-polygit/src/main/java/com/vaadin/hummingbird/demo/webcomponent/polygit/MainView.java
@@ -13,61 +13,44 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.vaadin.hummingbird.demo.webcomponent.progressbubble;
+package com.vaadin.hummingbird.demo.webcomponent.polygit;
 
-import java.util.ArrayList;
+import java.time.LocalDate;
 
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.hummingbird.html.Button;
+import com.vaadin.hummingbird.demo.webcomponent.polygit.component.ProgressBubble;
 import com.vaadin.hummingbird.html.Div;
 import com.vaadin.hummingbird.router.RouterConfiguration;
 import com.vaadin.hummingbird.router.RouterConfigurator;
 import com.vaadin.hummingbird.router.View;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Component;
 
 /**
  * Example on how to use a web component.
  *
  * @author Vaadin Ltd
  */
-public class ProgressBubbleView extends Div implements View {
+public class MainView extends Div implements View {
 
     private static final String BACKGROUND = "background";
-    ArrayList<ProgressBubble> bubbles = new ArrayList<>();
 
     /**
      * Creates a new view instance.
      */
-    public ProgressBubbleView() {
-        ProgressBubble bubble = new ProgressBubble(0, 100);
+    public MainView() {
+        ProgressBubble bubble = new ProgressBubble(0, 365);
         bubble.getElement().getStyle().set(BACKGROUND, "green");
-        bubbles.add(bubble);
-        bubble = new ProgressBubble(0, 100);
-        bubble.getElement().getStyle().set(BACKGROUND, "red");
-        bubbles.add(bubble);
-        bubble = new ProgressBubble(0, 100);
-        bubble.getElement().getStyle().set(BACKGROUND, "blue");
-        bubbles.add(bubble);
-        bubble = new ProgressBubble(0, 100);
-        bubble.getElement().getStyle().set(BACKGROUND, "purple");
-        bubbles.add(bubble);
+        bubble.setValue(LocalDate.now().getDayOfYear());
+        add(text("Progress of this year"), bubble, new RangeBubble());
+    }
 
-        Button makeProgress = new Button("Make progress");
-        makeProgress.setId("makeProgress");
-        makeProgress.addClickListener(e -> {
-            bubbles.forEach(pb -> pb.setValue(pb.getValue() + 5));
-        });
-
-        Button increaseMax = new Button("Increase max value");
-        increaseMax.setId("increaseMax");
-        increaseMax.addClickListener(e -> {
-            bubbles.forEach(pb -> pb.setMax(pb.getMax() * 2));
-        });
-
-        add(makeProgress, increaseMax);
-        bubbles.forEach(this::add);
+    private static Component text(String string) {
+        Div d = new Div();
+        d.setText(string);
+        return d;
     }
 
     /**
@@ -84,7 +67,7 @@ public class ProgressBubbleView extends Div implements View {
     public static class RouterConf implements RouterConfigurator {
         @Override
         public void configure(RouterConfiguration configuration) {
-            configuration.setRoute("", ProgressBubbleView.class);
+            configuration.setRoute("", MainView.class);
         }
     }
 }
