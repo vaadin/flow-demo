62ea2b97b4eb20c247064489d3e4ea764aa6926a
Artur Signell <artur@vaadin.com>
2016-07-19 15:02:30 +0300

    Change progress bubble demo into a Polygit web component demo

---
+++ demo-web-component-using-polygit/src/main/java/com/vaadin/hummingbird/demo/webcomponent/polygit/RangeBubble.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.hummingbird.demo.webcomponent.polygit;
+
+import com.vaadin.annotations.Id;
+import com.vaadin.annotations.Uses;
+import com.vaadin.hummingbird.demo.webcomponent.polygit.component.ProgressBubble;
+import com.vaadin.hummingbird.demo.webcomponent.polygit.component.VaadinComboBox;
+import com.vaadin.hummingbird.template.model.TemplateModel;
+import com.vaadin.ui.Template;
+
+import elemental.json.Json;
+import elemental.json.JsonArray;
+
+/**
+ * A template using web components.
+ */
+@Uses(ProgressBubble.class)
+@Uses(VaadinComboBox.class)
+public class RangeBubble extends Template {
+
+    public interface RangeBubbleModel extends TemplateModel {
+        int getEnd();
+
+        void setEnd(int end);
+
+        int getValue();
+
+        void setValue(int value);
+    }
+
+    @Id("rangeEnd")
+    private VaadinComboBox rangeEnd;
+    @Id("value")
+    private VaadinComboBox value;
+    @Id("bubble")
+    private ProgressBubble bubble;
+
+    /**
+     * Creates the template instance.
+     */
+    public RangeBubble() {
+        getModel().setValue(50);
+        getModel().setEnd(80);
+
+        JsonArray values = Json.createArray();
+        for (int i = 0; i < 100; i += 5) {
+            values.set(values.length(), String.valueOf(i));
+        }
+
+        rangeEnd.getElement().setPropertyJson("items", values);
+        rangeEnd.getElement().setProperty("value",
+                Integer.toString(getModel().getEnd()));
+        value.getElement().setPropertyJson("items", values);
+        value.getElement().setProperty("value",
+                Integer.toString(getModel().getValue()));
+        rangeEnd.getElement().addEventListener("value-changed", e -> {
+            getModel().setEnd(Integer
+                    .parseInt(e.getEventData().getString("element.value")));
+        }, "element.value");
+        value.getElement().addEventListener("value-changed", e -> {
+            getModel().setValue(Integer
+                    .parseInt(e.getEventData().getString("element.value")));
+        }, "element.value");
+    }
+
+    @Override
+    protected RangeBubbleModel getModel() {
+        return (RangeBubbleModel) super.getModel();
+    }
+}
