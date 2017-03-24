3b8b49265fc590f6ee1dfc902fe7f3663324be2b
Gilberto Torrezan <gilberto@vaadin.com>
2017-03-24 10:47:21 +0200

    Add integration test for table selection and jQuery table sorting.

--- demo-jquery-table/src/test/java/com/vaadin/hummingbird/demo/page/ReportsOverviewIT.java
+++ demo-jquery-table/src/test/java/com/vaadin/hummingbird/demo/page/ReportsOverviewIT.java
@@ -1,5 +1,9 @@
 package com.vaadin.hummingbird.demo.page;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -19,15 +23,76 @@ public class ReportsOverviewIT extends AbstractDemoTest {
         WebElement firstRow = table
                 .findElement(By.cssSelector("tbody tr:first-child"));
 
-        String classes = firstRow.getAttribute("class");
-        // ensure that the row is not selected at this stage
-        Assert.assertFalse(classes.contains(SELECTED_CSS_CLASS));
+        String firstRowClasses = firstRow.getAttribute("class");
+        // ensure that the first row is not selected at this stage
+        Assert.assertFalse(firstRowClasses.contains(SELECTED_CSS_CLASS));
         firstRow.click();
 
         waitForElementPresent(By.className("selected-report"));
-        classes = firstRow.getAttribute("class");
-        // ensure that the row is selected after the click
-        Assert.assertTrue(classes.contains(SELECTED_CSS_CLASS));
+        firstRowClasses = firstRow.getAttribute("class");
+        // ensure that the first row is selected after the click
+        Assert.assertTrue(firstRowClasses.contains(SELECTED_CSS_CLASS));
+
+        // verifying the contents of the dialog
+        WebElement firstCell = firstRow
+                .findElement(By.cssSelector("td:first-child"));
+        String selectedId = firstCell.getText();
+        WebElement dialog = findElement(By.className("selected-report"));
+        Assert.assertTrue(dialog.getText().contains("#" + selectedId));
+
+        // selecting the next row
+        WebElement secondRow = table
+                .findElement(By.cssSelector("tbody tr:nth-child(2)"));
+        secondRow.click();
+        firstRowClasses = firstRow.getAttribute("class");
+
+        // ensure that the first row is deselected after the click
+        Assert.assertFalse(firstRowClasses.contains(SELECTED_CSS_CLASS));
+        String secondRowClasses = secondRow.getAttribute("class");
+
+        // ensure that the second row is selected after the click
+        Assert.assertTrue(secondRowClasses.contains(SELECTED_CSS_CLASS));
+
+        // verifying the contents of the dialog
+        WebElement secondCell = secondRow
+                .findElement(By.cssSelector("td:first-child"));
+        selectedId = secondCell.getText();
+        dialog = findElement(By.className("selected-report"));
+        Assert.assertTrue(dialog.getText().contains("#" + selectedId));
+
+        // deselecting
+        secondRow.click();
+        waitForElementNotPresent(By.className("selected-report"));
+        secondRowClasses = secondRow.getAttribute("class");
+        Assert.assertFalse(secondRowClasses.contains(SELECTED_CSS_CLASS));
     }
 
+    @Test
+    public void testJQuerySorting() {
+        open();
+        waitForElementPresent(By.tagName("table"));
+
+        WebElement table = findElement(By.tagName("table"));
+        List<WebElement> rows = table.findElements(By.cssSelector("tbody tr"));
+        List<Integer> idsBeforeJQuerySort = new ArrayList<>(rows.size());
+        for (WebElement row : rows) {
+            WebElement cell = row.findElement(By.cssSelector("td:first-child"));
+            idsBeforeJQuerySort.add(Integer.parseInt(cell.getText()));
+        }
+
+        // clicking on the header to make the jquery sort the by the column
+        WebElement firstHeader = table
+                .findElement(By.cssSelector("thead th:first-child"));
+        firstHeader.click();
+
+        rows = table.findElements(By.cssSelector("tbody tr"));
+        List<Integer> idsAfterJQuerySort = new ArrayList<>(rows.size());
+        for (WebElement row : rows) {
+            WebElement cell = row.findElement(By.cssSelector("td:first-child"));
+            idsAfterJQuerySort.add(Integer.parseInt(cell.getText()));
+        }
+        // sort the first list and compare it with the list sorted by jquery
+        Collections.sort(idsBeforeJQuerySort);
+        Assert.assertTrue(idsBeforeJQuerySort.equals(idsAfterJQuerySort));
+    }
 }
