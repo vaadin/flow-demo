2e6d13a578dd6d1a985b576b59293a0237833796
Kirill Bulatov <mail4score@gmail.com>
2017-03-24 09:41:55 +0200

    Added tests for Polymer minesweeper. (#226)

---
+++ demo-minesweeper-polymer/src/test/java/com/vaadin/hummingbird/demo/minesweeper/component/MinesweeperIT.java
@@ -0,0 +1,38 @@
+package com.vaadin.hummingbird.demo.minesweeper.component;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MinesweeperIT extends MinesweeperPageObject {
+
+    @Test
+    public void clickOnEmptyRevealsCell() {
+        open("seed=1");
+        assertTrue(isCellHidden(0, 0));
+        getCell(0, 0).click();
+        assertTrue(isCellEmpty(0, 0));
+    }
+
+    @Test
+    public void clickOnMineShowsNotification() {
+        open("seed=1");
+        getCell(9, 0).click();
+        waitForBoomNotification();
+    }
+
+    @Test
+    public void solveSeed1Minefield() {
+        open("seed=1&rows=10&cols=10");
+        for (int row = 0; row < 10; row++) {
+            for (int col = 0; col < 10; col++) {
+                if (!isMineSeed1(row, col)) {
+                    assertTrue(row + "," + col + " is revealed or a mine",
+                            isCellHidden(row, col) || isCellEmpty(row, col));
+                    getCell(row, col).click();
+                }
+            }
+        }
+        waitForSuccessNotification();
+    }
+}
