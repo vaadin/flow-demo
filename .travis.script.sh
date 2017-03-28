43bd5971588ce209f9f386a55b67c0f043b55886
Kirill Bulatov <mail4score@gmail.com>
2017-03-28 15:01:49 +0300

    Replaced tabs with spaces.

--- .travis.script.sh
+++ .travis.script.sh
@@ -12,11 +12,11 @@ export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]
 then
-	# Pull request inside repository, secure vars available (needed for Sonar and TestBench tests)
-	# Run verify + Sonar analysis
-	mvn -B -e -V -Dmaven.javadoc.skip=false -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Dtest.excludegroup= -Dsonar.verbose=true -Dsonar.analysis.mode=issues -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.host.url=$SONAR_HOST -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH -Dsonar.login=$SONAR_LOGIN -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST -Dsonar.exclusions=$SONAR_EXCLUSIONS clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar
+    # Pull request inside repository, secure vars available (needed for Sonar and TestBench tests)
+    # Run verify + Sonar analysis
+    mvn -B -e -V -Dmaven.javadoc.skip=false -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Dtest.excludegroup= -Dsonar.verbose=true -Dsonar.analysis.mode=issues -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.host.url=$SONAR_HOST -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH -Dsonar.login=$SONAR_LOGIN -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST -Dsonar.exclusions=$SONAR_EXCLUSIONS clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar
 elif [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]
 then
-	# Branch build, secure vars available (needed for TestBench tests)
-	mvn -B -e -V -Dmaven.javadoc.skip=false -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Dsonar.exclusions=$SONAR_EXCLUSIONS -Dtest.excludegroup= verify
+    # Branch build, secure vars available (needed for TestBench tests)
+    mvn -B -e -V -Dmaven.javadoc.skip=false -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Dsonar.exclusions=$SONAR_EXCLUSIONS -Dtest.excludegroup= verify
 fi
