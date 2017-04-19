f5ec6a29457cca1730a9e19176ceaff4c5db905b
Gilberto Torrezan <gilberto@vaadin.com>
2017-04-19 16:32:19 +0300

    Fix compilation errors due to the change on the @WebComponents annotation.

--- demo-minesweeper-polymer/src/main/java/com/vaadin/flow/demo/minesweeper/component/MinesweeperUI.java
+++ demo-minesweeper-polymer/src/main/java/com/vaadin/flow/demo/minesweeper/component/MinesweeperUI.java
@@ -21,6 +21,7 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.WebComponents;
+import com.vaadin.annotations.WebComponents.PolyfillVersion;
 import com.vaadin.flow.demo.minesweeper.component.component.MinesweeperComponent;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
@@ -28,10 +29,10 @@ import com.vaadin.ui.Component;
 import com.vaadin.ui.UI;
 
 /**
- * UI which demonstrates how you can use the Flow {@link Component} API
- * to create a Minesweeper game.
+ * UI which demonstrates how you can use the Flow {@link Component} API to
+ * create a Minesweeper game.
  */
-@WebComponents(1)
+@WebComponents(PolyfillVersion.V1)
 public class MinesweeperUI extends UI {
 
     /**
