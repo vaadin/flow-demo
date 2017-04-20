9da8df78abc84ef786fd8d2e37fc9ee297f85452
Gilberto Torrezan <gilberto-torrezan@users.noreply.github.com>
2017-04-20 08:47:54 +0300

    Fix compilation errors due to the change on the @WebComponents (#250)
    * Fix compilation errors due to the change on the @WebComponents
    annotation.

--- demo-hello-world-template/src/main/java/com/vaadin/flow/demo/helloworld/template/Servlet.java
+++ demo-hello-world-template/src/main/java/com/vaadin/flow/demo/helloworld/template/Servlet.java
@@ -19,6 +19,7 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.WebComponents;
+import com.vaadin.annotations.WebComponents.PolyfillVersion;
 import com.vaadin.flow.demo.helloworld.template.Servlet.MyRouterConfigurator;
 import com.vaadin.flow.router.RouterConfiguration;
 import com.vaadin.flow.router.RouterConfigurator;
@@ -29,7 +30,7 @@ import com.vaadin.server.VaadinServlet;
  */
 @WebServlet(urlPatterns = "/*", name = "UIServlet", asyncSupported = true)
 @VaadinServletConfiguration(routerConfigurator = MyRouterConfigurator.class, productionMode = false)
-@WebComponents(1)
+@WebComponents(PolyfillVersion.V1)
 public class Servlet extends VaadinServlet {
 
     /**
