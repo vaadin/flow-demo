05fdb1e023c18764c8dc33b03e400d7d178a681d
Denis <denis@vaadin.com>
2017-07-18 09:39:16 +0300

    Create model converters demo project (#311)
    * Create model converters demo project
    
    Fixes #308
    
    * Create model converters demo project
    
    Fixes #308

---
+++ demo-model-converters/src/main/java/com/vaadin/flow/demo/helloworld/template/SignInForm.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.demo.helloworld.template;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.logging.Logger;
+
+import com.vaadin.annotations.Convert;
+import com.vaadin.annotations.EventHandler;
+import com.vaadin.annotations.HtmlImport;
+import com.vaadin.annotations.Tag;
+import com.vaadin.flow.demo.helloworld.template.SignInForm.SignInModel;
+import com.vaadin.flow.router.View;
+import com.vaadin.flow.template.PolymerTemplate;
+import com.vaadin.flow.template.model.TemplateModel;
+
+/**
+ * The one and only view in the SignIn application.
+ */
+@Tag("sign-in-form")
+@HtmlImport("frontend://components/SignInForm.html")
+public class SignInForm extends PolymerTemplate<SignInModel> implements View {
+
+    /**
+     * The template model.
+     */
+    public interface SignInModel extends TemplateModel {
+
+        /**
+         * Gets the name of a registrant.
+         *
+         * @return the name of a registrant
+         */
+        String getName();
+
+        /**
+         * Sets the name of a registrant.
+         *
+         * @param name
+         *            the name of a registrant
+         */
+        void setName(String name);
+
+        /**
+         * Social security number. It has {@link Long} type and is supposed to
+         * be stored as a {@link Long} in a database.
+         * <p>
+         * Flow doesn't support {@link Long} since this type doesn't exist on
+         * the client side. So it should be converted to some supported type.
+         *
+         * @param ssd
+         *            the social security number of a registrant
+         */
+        @Convert(LongToStringConverter.class)
+        void setSsd(Long ssd);
+
+        /**
+         * Gets the name of a registrant.
+         *
+         * @return the social sceurity number of a registrant
+         */
+        Long getSsd();
+
+        /**
+         * Returns the date of birth.
+         * <p>
+         * One {@link Date} value will be used in 3 different input fields: a
+         * separate day field, a month field and an year field. So converter is
+         * used here to convert one value into a bean with 3 properties.
+         *
+         * @param date
+         *            the date of birth
+         */
+        @Convert(DateToDateBeanConverter.class)
+        void setBirthDate(Date date);
+
+        /**
+         * Gets the the date of birth.
+         *
+         * @return the date of birth
+         */
+        Date getBirthDate();
+
+        /**
+         * Sets the registration resulting message.
+         *
+         * @param msg
+         *            a message
+         */
+        void setRegistrationMessage(String msg);
+    }
+
+    /**
+     * Creates a new instance of the sign in form.
+     */
+    public SignInForm() {
+        setId("template");
+        getModel().setBirthDate(new Date());
+    }
+
+    @EventHandler
+    private void signIn() {
+        String name = getModel().getName();
+        Long ssd = getModel().getSsd();
+        Date date = getModel().getBirthDate();
+        Logger.getLogger(SignInForm.class.getName())
+                .info("Register a new user with the name '" + name
+                        + "; and  SSD  '" + ssd + "', and birthday: " + date);
+        Calendar calendar = GregorianCalendar.getInstance();
+        calendar.setTime(date);
+
+        LocalDate birthDay = calendar.toInstant().atZone(ZoneId.systemDefault())
+                .toLocalDate();
+        getModel().setRegistrationMessage("Welcome " + name + ", your are "
+                + Period.between(birthDay, LocalDate.now()).getYears()
+                + " years old");
+    }
+}
---
+++ demo-model-converters/src/test/java/com/vaadin/flow/demo/helloworld/template/SignInFormIT.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.demo.helloworld.template;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.flow.demo.testutil.AbstractChromeTest;
+import com.vaadin.testbench.By;
+
+public class SignInFormIT extends AbstractChromeTest {
+
+    @Test
+    public void basicFunctionality() {
+        open();
+
+        setText("name", "John Doe");
+        setText("day", "2");
+        setText("month", "4");
+        setText("year", "1975");
+
+        WebElement template = findElement(By.id("template"));
+        getInShadowRoot(template, By.id("register")).click();
+        WebElement msg = getInShadowRoot(template, By.id("reg-msg"));
+
+        LocalDate date = LocalDate.of(1975, 4, 2);
+        waitUntil(driver -> msg.getText()
+                .equals("Welcome John Doe, your are "
+                        + Period.between(date, LocalDate.now()).getYears()
+                        + " years old"));
+    }
+
+    private void setText(String inputId, String text) {
+        WebElement template = findElement(By.id("template"));
+
+        WebElement input = getInShadowRoot(template, By.id(inputId));
+        WebElement nativeInput = getInShadowRoot(input, By.id("nativeInput"));
+        nativeInput.clear();
+        nativeInput.sendKeys(text);
+    }
+}
