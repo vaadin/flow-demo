5ace12bbb3b370ee4c1cd7c9f94371f4deca2ea1
Kirill Bulatov <mail4score@gmail.com>
2017-08-07 11:57:56 +0300

    Build script now supports both Selenoid and local Chrome for IT tests.

--- .travis.script.sh
+++ .travis.script.sh
@@ -4,73 +4,89 @@
 # TRAVIS_BRANCH == target of normal commit or target of PR
 # TRAVIS_SECURE_ENV_VARS == true if encrypted variables, e.g. SONAR_HOST is available
 # TRAVIS_REPO_SLUG == the repository, e.g. vaadin/vaadin
+# SKIP_SONAR == skip sonar checks
+# USE_SELENOID == use Docker and Selenoid to run IT tests in personal hub
 
 # Exclude the bower_components and node_modules from Sonar analysis
 SONAR_EXCLUSIONS=**/bower_components/**,**/node_modules/**,**/node/**
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]
-then
+function getDockerParamsIfNeeded {
+    if [ "$USE_SELENOID" == "true" ]
+    then
+        echo "-Dtest.use.hub=true \
+            -Dcom.vaadin.testbench.Parameters.hubHostname=localhost"
+    else
+        echo ""
+    fi
+}
+
+function getSonarDetails {
+    if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+    then
+        echo "-Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+               -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
+               -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+               -Dsonar.analysis.mode=issues"
+    else
+        echo "-Dsonar.analysis.mode=publish"
+    fi
+}
+
+function runSonar {
     if [ "$SKIP_SONAR" != "true" ]
     then
-        # Pull request inside repository, secure vars available (needed for Sonar and TestBench tests)
-        # Run verify + Sonar analysis
+        # Sonar should be run after the project is built so that findbugs can analyze compiled sources
+        echo "Running Sonar"
         mvn -B -e -V \
             -Dmaven.javadoc.skip=false \
             -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE \
-            -Dtest.excludegroup= \
-            -Dtest.use.hub=true \
-            -Dcom.vaadin.testbench.Parameters.hubHostname="localhost" \
-            -Dsonar.verbose=true \
-            -Dsonar.analysis.mode=issues \
-            -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-            -Dsonar.host.url=$SONAR_HOST \
-            -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
-            -Dsonar.login=$SONAR_LOGIN \
-            -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
             -Dsonar.exclusions=$SONAR_EXCLUSIONS \
-            clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar
+            -Dsonar.verbose=true \
+            -Dsonar.host.url=$SONAR_HOST \
+            -Dsonar.login=$SONAR_LOGIN \
+            $(getSonarDetails) \
+            -DskipTests \
+            compile sonar:sonar
     else
-        # Run verify and skip sonar
-        mvn -B -e -V \
-            -Dmaven.javadoc.skip=false \
-            -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE \
-            -Dtest.excludegroup= \
-            -Dtest.use.hub=true \
-            -Dcom.vaadin.testbench.Parameters.hubHostname="localhost" \
-            clean org.jacoco:jacoco-maven-plugin:prepare-agent verify
+        echo "SKIP_SONAR env variable is set to 'true', skipping sonar."
+    fi
+}
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]
+then
+    # Pull request inside repository, secure vars available (needed for Sonar and TestBench tests)
+    mvn -B -e -V \
+        -Dmaven.javadoc.skip=false \
+        -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE \
+        -Dtest.excludegroup= \
+        $(getDockerParamsIfNeeded) \
+        clean org.jacoco:jacoco-maven-plugin:prepare-agent verify
+
+    # Get the status for the previous maven command and if not exception then run sonar.
+    STATUS=$?
+    if [ $STATUS -eq 0 ]
+    then
+        runSonar
+    else
+        echo "Build failed, skipping sonar."
+        exit 1
     fi
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]
 then
     # master build
     mvn -B -e -V \
         -Pall-tests \
-        -Dtest.use.hub=true \
-        -Dcom.vaadin.testbench.Parameters.hubHostname="localhost" \
+        $(getDockerParamsIfNeeded) \
         -Dvaadin.productionMode=true \
         -Dmaven.javadoc.skip=false \
         -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE \
         clean org.jacoco:jacoco-maven-plugin:prepare-agent install
 
+    # Get the status for the previous maven command and if not exception then run sonar.
     STATUS=$?
     if [ $STATUS -eq 0 ]
     then
-        if [ "$SKIP_SONAR" != "true" ]
-        then
-            # run sonar
-            echo "Running Sonar"
-            mvn -B -e -V \
-                -Dmaven.javadoc.skip=false \
-                -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE \
-                -Dsonar.analysis.mode=publish \
-                -Dsonar.exclusions=$SONAR_EXCLUSIONS \
-                -Dsonar.verbose=true \
-                -Dsonar.host.url=$SONAR_HOST \
-                -Dsonar.login=$SONAR_LOGIN \
-                -DskipTests \
-                compile sonar:sonar
-        else
-            echo "Skipping sonar."
-        fi
+       runSonar
     else
         echo "Build failed, skipping sonar."
         exit 1
@@ -82,7 +98,6 @@ else
         -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE \
         -Dsonar.exclusions=$SONAR_EXCLUSIONS \
         -Dtest.excludegroup= \
-        -Dtest.use.hub=true \
-        -Dcom.vaadin.testbench.Parameters.hubHostname="localhost" \
+        $(getDockerParamsIfNeeded) \
         verify
 fi
