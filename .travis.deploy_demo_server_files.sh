02714efa894bbf651b9576ab2d23fe1b423b5e21
Kirill Bulatov <mail4score@gmail.com>
2017-08-30 10:21:54 +0300

    Now all demo files are synced.

--- .travis.deploy_demo_server_files.sh
+++ .travis.deploy_demo_server_files.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-scp ./demo-server/flow-logo.svg flow:~/tomcat/webapps/ROOT/flow-logo.svg
-scp ./demo-server/index.jsp flow:~/tomcat/webapps/ROOT/index.jsp
+scp ./demo-server/* flow:~/tomcat/webapps/ROOT/
