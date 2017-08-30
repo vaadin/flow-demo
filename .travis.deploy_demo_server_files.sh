cdd7dbaeddd8e0ba0b23ac599191f9bd6e51e9c1
Kirill Bulatov <mail4score@gmail.com>
2017-08-30 10:38:02 +0300

    Add a documentation link and fix the deploy script to publish updated logo. (#337)

--- .travis.deploy_demo_server_files.sh
+++ .travis.deploy_demo_server_files.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-scp ./demo-server/bird.png flow:~/tomcat/webapps/ROOT/bird.png
-scp ./demo-server/index.jsp flow:~/tomcat/webapps/ROOT/index.jsp
+scp ./demo-server/* flow:~/tomcat/webapps/ROOT/
