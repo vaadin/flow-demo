a4dc52d688fc74fd6c9551ba03b5ec7ec75b204f
Mikael Grankvist <mikael.grankvist@vaadin.com>
2017-10-02 14:20:40 +0300

    Test that blog post list lists blog posts.

--- demo-static-menu-router/src/test/java/com/vaadin/flow/demo/website/BlogPostIT.java
+++ demo-static-menu-router/src/test/java/com/vaadin/flow/demo/website/BlogPostIT.java
@@ -8,6 +8,15 @@ import com.vaadin.testbench.By;
 public class BlogPostIT extends AbstractComplexStaticMenuTest {
 
     @Test
+    public void blog_list_visible() {
+        openRouteUrl("blog");
+        
+        Assert.assertTrue("Blog post 1 link not present", this.isElementPresent(By.xpath("//a[contains(@href,'blog/1')]")));
+        Assert.assertTrue("Blog post 2 link not present", this.isElementPresent(By.xpath("//a[contains(@href,'blog/2')]")));
+        Assert.assertTrue("Blog post 3 link not present", this.isElementPresent(By.xpath("//a[contains(@href,'blog/3')]")));
+    }
+
+    @Test
     public void valid_blog_post_url() {
         openRouteUrl("blog/1");
         Assert.assertTrue("Blog post content should be present",
