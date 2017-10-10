56dab86c665fccde9ee98b1201bb5473447c5e55
Kirill Bulatov <kirill@vaadin.com>
2017-10-10 16:34:56 +0300

    Remove Travis mentions.

--- .travis.deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]
-then
-    ./.travis.deploy_demo_server_files.sh
-    # Copy demos to the server
-    scp -o "StrictHostKeyChecking no" -P 5177 */target/*.war dev@virtuallypreinstalled.com:tomcat/webapps/
-fi
