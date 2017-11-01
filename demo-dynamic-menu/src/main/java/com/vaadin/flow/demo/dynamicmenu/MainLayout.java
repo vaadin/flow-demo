852818885815cb2dc943dda6cca26e70518d6b06
Mikael Grankvist <mikael.grankvist@vaadin.com>
2017-11-01 14:49:49 +0200

    Remove the router configuration.
    clean imports.

--- demo-dynamic-menu/src/main/java/com/vaadin/flow/demo/dynamicmenu/MainLayout.java
+++ demo-dynamic-menu/src/main/java/com/vaadin/flow/demo/dynamicmenu/MainLayout.java
@@ -15,12 +15,7 @@
  */
 package com.vaadin.flow.demo.dynamicmenu;
 
-import com.vaadin.flow.dom.Element;
-import com.vaadin.flow.router.LocationChangeEvent;
-import com.vaadin.flow.router.View;
 import com.vaadin.router.RouterLayout;
-import com.vaadin.router.event.BeforeEnterObserver;
-import com.vaadin.router.event.BeforeNavigationEvent;
 import com.vaadin.ui.common.StyleSheet;
 import com.vaadin.ui.html.Div;
 
