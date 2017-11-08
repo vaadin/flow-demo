b91a94658da50179e4e8747e64a714cb4e123057
Artur Signell <artur@vaadin.com>
2017-11-08 10:44:59 +0200

    Change UI to a view

---
+++ demo-minesweeper-polymer/src/main/java/com/vaadin/flow/demo/minesweeper/component/MinesweeperView.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.demo.minesweeper.component;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.vaadin.flow.demo.minesweeper.component.component.MinesweeperComponent;
+import com.vaadin.router.Route;
+import com.vaadin.router.event.BeforeEnterObserver;
+import com.vaadin.router.event.BeforeNavigationEvent;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.html.Div;
+
+/**
+ * UI which demonstrates how you can use the Flow {@link Component} API to
+ * create a Minesweeper game.
+ */
+@Route(value = "")
+public class MinesweeperView extends Div implements BeforeEnterObserver {
+
+    private static int getParam(BeforeNavigationEvent event, String name,
+            int defaultValue) {
+        List<String> param = event.getLocation().getQueryParameters()
+                .getParameters().get(name);
+        return Optional.ofNullable(param).map(l -> l.get(0))
+                .map(Integer::parseInt).orElse(defaultValue);
+    }
+
+    @Override
+    public void beforeEnter(BeforeNavigationEvent event) {
+        long seed = getParam(event, "seed", (int) System.currentTimeMillis());
+        double mineDensity = getParam(event, "mineDensity", 20) / 100.0;
+        int rows = getParam(event, "rows", 10);
+        int cols = getParam(event, "cols", 10);
+
+        add(new MinesweeperComponent(seed, mineDensity, rows, cols));
+    }
+
+}
