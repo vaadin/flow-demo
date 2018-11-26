e00c4eba391bffb39320887fd740391c16241cc1
Mikael Grankvist <mikael.grankvist@vaadin.com>
2018-11-26 13:01:46 +0200

    Dynamic route demo with no backing functionality in Flow.

---
+++ demo-dynamic-routes/src/main/java/com/vaadin/flow/demo/Login.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.demo;
+
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.demo.dynamic.AdminView;
+import com.vaadin.flow.demo.dynamic.UserView;
+import com.vaadin.flow.demo.dynamic.VersionView;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinSession;
+import com.vaadin.flow.server.startup.RouteRegistry;
+
+@Route("")
+public class Login extends Div {
+
+    Span message;
+    TextField login;
+    PasswordField password;
+
+    public Login() {
+        message = new Span();
+        message.setVisible(false);
+
+        login = new TextField("Login");
+        password = new PasswordField("Password");
+
+        Button submit = new Button("Submit", this::handeLogin);
+
+        add(message, login, password, submit);
+    }
+
+    private void handeLogin(ClickEvent<Button> buttonClickEvent) {
+        clearMessage();
+        if (login.isEmpty() || password.isEmpty()) {
+            setMessage("All fields are required.");
+        }
+
+        // super secure
+        if("admin".equals(login.getValue())) {
+            // A single Registry should be available that should handle all scopes. Single entrypoint from the UI.
+            RouteRegistry sessionRegistry = RouteRegistry.get(VaadinSession.getCurrent());
+            // Set route should override global route, but throw if session contains same route.
+            sessionRegistry.setRoute(AdminView.class);
+            UI.getCurrent().navigate("");
+        } else if ("user".equals(login.getValue())) {
+            RouteRegistry sessionRegistry = RouteRegistry.get(VaadinSession.getCurrent());
+            // Set route should override global route, but throw if session contains same route.
+            sessionRegistry.setRoute(UserView.class);
+            // navigating to where we are should work as setRoute should clear the lastNavigated flag for the UI
+            // as the current path may have changed to be something else.
+            UI.getCurrent().navigate("");
+        } else {
+            setMessage("Faulty username or password!");
+            return;
+        }
+
+        RouteRegistry sessionRegistry = RouteRegistry.get(VaadinSession.getCurrent());
+        // Add the version view to the route for path "version" with the MainLayout as its parent.
+        // Note that the parent routes shouldn't be as a list as we can collect parents using
+        // RouterUtil.getParentLayoutsForNonRouteTarget(MainLayout.class), though this
+        // depends on how dynamic do we want to support. We should anyway be able to request
+        // registry for the parts that we need for navigation.
+        sessionRegistry.addRoute("version", VersionView.class, MainLayout.class);
+
+    }
+
+    private void setMessage(String messageText) {
+        message.setVisible(true);
+        message.setText(messageText);
+    }
+
+    private void clearMessage() {
+        message.setVisible(false);
+    }
+
+
+    public static void logout() {
+        RouteRegistry sessionRegistry = RouteRegistry.get(VaadinSession.getCurrent());
+        // clear any session routes.
+        sessionRegistry.clear();
+        // navigating to where we are should work as setRoute should clear the lastNavigated flag for the UI
+        // as the current path may have changed to be something else.
+        UI.getCurrent().navigate("");
+    }
+}
---
+++ demo-dynamic-routes/src/main/java/com/vaadin/flow/demo/dynamic/AdminView.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.demo.dynamic;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.demo.Login;
+import com.vaadin.flow.demo.MainLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
+
+/**
+ * Admin view that can be dynamically registered.
+ *
+ * The @DynamicRoute annotation removes this class from the initial registration
+ * and lets us register it later with the default functionality expected of a
+ *
+ * @Route
+ */
+@Route(value = "", layout = MainLayout.class)
+@DynamicRoute
+public class AdminView extends Div {
+
+    public AdminView() {
+        Span text = new Span("This is the view for a logged in admin.");
+        RouterLink version = new RouterLink("Version", VersionView.class);
+        Button logout = new Button("Logout", e -> Login.logout());
+
+        add(text, version, logout);
+    }
+}
